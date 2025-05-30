bf7b33dc64b700845ec5f64c9f542ad01cf7aeae
GageGotya <gage.gotya@gmail.com>
2025-05-30 00:07:20 -0500

    fix: remove user prop from DashboardNav component

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -29,7 +29,7 @@ export default function DashboardLayout({
 
   return (
     <div className="min-h-screen bg-gray-50">
-      <DashboardNav user={user} />
+      <DashboardNav />
       <main className="py-10">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           {children}
